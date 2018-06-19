263367c44a36b0d15ffee993b04b3fac579dcba2
Max Appleton <maxiappleton@gmail.com>
2018-06-19 17:32:24 -0500

    updates products

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -42,22 +42,36 @@ class Shop extends Component {
 
   addToCart = (id) => {
     let updatedCart;
+
     if (this.state.cart.some(product => product.id === id)) {
       updatedCart = this.state.cart.map(product => {
         if (product.id === id) {
-          return Object.assign({}, product, {quantity: product.quantity + 1})
+          return Object.assign({}, product, { quantity: product.quantity + 1 })
         } else {
           return product;
         }
       })
     } else {
-      const product = this.state.products.filter(p => p.id === id);
-      const productToAdd = Object.assign({}, product, {quantity: 1})
-      updatedCart = this.state.cart.concat(productToAdd)
+      const product = this.state.products.filter(p => p.id === id)[0];
+      const productToAdd = Object.assign({}, product, { quantity: 1 });
+      console.log(productToAdd);
+      updatedCart = this.state.cart.concat(productToAdd);
     }
+
+    const updatedProducts = this.state.products.map((product) => {
+      if (product.id === id) {
+        return Object.assign({}, product, {
+          quantity: product.quantity - 1,
+        });
+      } else {
+        return product;
+      }
+    });
+
     this.setState(prevState => {
       return {
-        cart: updatedCart
+        cart: updatedCart,
+        products: updatedProducts
       }
     })
   }
