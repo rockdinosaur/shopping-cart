65919bd6c7aa36e2e1cf1dd7091819b0586a4f89
Max Appleton <maxiappleton@gmail.com>
2018-06-19 17:53:46 -0500

    fixed product quantity 0

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Product extends Component {
   handleAddToCart = () => {
-    this.props.onAddToCartClick(this.props.id);
+    this.props.quantity && this.props.onAddToCartClick(this.props.id);
   }
 
   render() {
+    const addToCartClasses = `button add-to-cart ${this.props.quantity ? '' : 'disabled'}`;
+
     return (
       <div class="product">
         <div class="product-details">
@@ -14,7 +16,7 @@ class Product extends Component {
           <p class="quantity">{this.props.quantity} left in stock</p>
           <div class="actions product-actions">
             <a
-              class="button add-to-cart"
+              class={addToCartClasses}
               onClick={this.handleAddToCart}
             >Add to Cart</a>
             <a class="button edit">Edit</a>
