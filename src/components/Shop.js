9dee8690447bdfc70756d7b82e13007005657111
Steven Shen <steeve.shen@gmail.com>
2018-06-19 18:18:22 -0400

    updated cart

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -41,16 +41,26 @@ class Shop extends Component {
   };
 
   addToCart = (id) => {
-    this.state.products.forEach((product) => {
-      if (product.id === id) {
-        this.setState(prevState => {
-          return {
-            cart: prevState.cart.concat(product)
-          }
-        });
+    let updatedCart;
+    if (this.state.cart.some(product => product.id === id)) {
+      updatedCart = this.state.cart.map(product => {
+        if (product.id === id) {
+          return Object.assign({}, product, {quantity: product.quantity + 1})
+        } else {
+          return product;
+        }
+      })
+    } else {
+      const product = this.state.products.filter(p => p.id === id);
+      const productToAdd = Object.assign({}, product, {quantity: 1})
+      updatedCart = this.state.cart.concat(productToAdd)
+    }
+    this.setState(prevState => {
+      return {
+        cart: updatedCart
       }
     })
-  };
+  }
 
   render() {
     return (
