f895ae7ba74e50527c3a23c2a6fc5ae9ce9c4a32
Steven Shen <steeve.shen@gmail.com>
2018-06-19 19:23:59 -0400

    completed checkout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import Cart from './Cart';
 
 class Header extends Component {
+  handleCheckoutClick = () => {
+    this.props.handleCheckoutClick;
+  }
+
   render() {
     return (
       <header>
         <h1>The Shop!</h1>
-        <Cart 
+        <Cart
           cartItems={this.props.cartItems}
+          handleCheckoutClick={this.props.handleCheckoutClick}
         />
       </header>
     )
